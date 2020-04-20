d1c4b166cdafbaf13e361cdada9ba8fe805f6d12
Paul Roth <paulroth3d@gmail.com>
2020-04-20 13:32:42 -0500

    UPDATE: add in the attachment command

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ $ npm install -g heroku-usage-plugin
 $ heroku COMMAND
 running command...
 $ heroku (-v|--version|version)
-heroku-usage-plugin/1.2.0 darwin-x64 node-v12.16.1
+heroku-usage-plugin/1.3.0 darwin-x64 node-v12.16.1
 $ heroku --help [COMMAND]
 USAGE
   $ heroku COMMAND
@@ -37,6 +37,7 @@ USAGE
 * [`heroku usage:status`](#heroku-usagestatus)
 * [`heroku usage:status:addon`](#heroku-usagestatusaddon)
 * [`heroku usage:status:app`](#heroku-usagestatusapp)
+* [`heroku usage:status:attachment`](#heroku-usagestatusattachment)
 * [`heroku usage:status:dyno`](#heroku-usagestatusdyno)
 
 ## `heroku usage:daily`
@@ -67,7 +68,7 @@ DESCRIPTION
   https://devcenter.heroku.com/articles/platform-api-reference#enterprise-account-daily-usage
 ```
 
-_See code: [src/commands/usage/daily.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.2.0/src/commands/usage/daily.js)_
+_See code: [src/commands/usage/daily.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.3.0/src/commands/usage/daily.js)_
 
 ## `heroku usage:list:apps`
 
@@ -86,7 +87,7 @@ DESCRIPTION
   (If no user or term is specified, then the apps available to the current user is provided)
 ```
 
-_See code: [src/commands/usage/list/apps.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.2.0/src/commands/usage/list/apps.js)_
+_See code: [src/commands/usage/list/apps.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.3.0/src/commands/usage/list/apps.js)_
 
 ## `heroku usage:monthly`
 
@@ -118,7 +119,7 @@ DESCRIPTION
   https://devcenter.heroku.com/articles/platform-api-reference#enterprise-account-monthly-usage
 ```
 
-_See code: [src/commands/usage/monthly.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.2.0/src/commands/usage/monthly.js)_
+_See code: [src/commands/usage/monthly.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.3.0/src/commands/usage/monthly.js)_
 
 ## `heroku usage:status`
 
@@ -142,7 +143,7 @@ DESCRIPTION
   Please see those commands for more detail.
 ```
 
-_See code: [src/commands/usage/status/index.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.2.0/src/commands/usage/status/index.js)_
+_See code: [src/commands/usage/status/index.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.3.0/src/commands/usage/status/index.js)_
 
 ## `heroku usage:status:addon`
 
@@ -185,7 +186,7 @@ DESCRIPTION
   https://devcenter.heroku.com/articles/platform-api-reference#add-on
 ```
 
-_See code: [src/commands/usage/status/addon.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.2.0/src/commands/usage/status/addon.js)_
+_See code: [src/commands/usage/status/addon.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.3.0/src/commands/usage/status/addon.js)_
 
 ## `heroku usage:status:app`
 
@@ -217,7 +218,38 @@ DESCRIPTION
   https://devcenter.heroku.com/articles/platform-api-reference#app
 ```
 
-_See code: [src/commands/usage/status/app.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.2.0/src/commands/usage/status/app.js)_
+_See code: [src/commands/usage/status/app.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.3.0/src/commands/usage/status/app.js)_
+
+## `heroku usage:status:attachment`
+
+List of Add-Ons at the attachment level.
+
+```
+USAGE
+  $ heroku usage:status:attachment
+
+OPTIONS
+  -a, --app=app                comma separated list of app names or ids
+  -f, --format=human|json|csv  [default: human] format of output
+  -t, --team=team              team name or id
+  -u, --user=user              account email or user id
+
+DESCRIPTION
+  (Please note that if neither a team or user is specified, all apps for the current user are considered)
+
+  Attachment Name: Unique name for this add-on attachment to this app
+  Add-On Name: Globally unique name of the Add-On
+  Relationship: whether the attachment is owned by a different app (attached) or directly by the app (direct)
+  Owning App: Name of the singular app that owns the Add-On
+  Attached App: Name of the app that this add-on is attached to
+  Created At: the date the attachment was created
+  Updated At: the date the attachment was updated
+
+  For more information, please see:
+  https://devcenter.heroku.com/articles/platform-api-reference#add-on-attachment
+```
+
+_See code: [src/commands/usage/status/attachment.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.3.0/src/commands/usage/status/attachment.js)_
 
 ## `heroku usage:status:dyno`
 
@@ -254,5 +286,5 @@ DESCRIPTION
   https://devcenter.heroku.com/articles/platform-api-reference#dyno
 ```
 
-_See code: [src/commands/usage/status/dyno.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.2.0/src/commands/usage/status/dyno.js)_
+_See code: [src/commands/usage/status/dyno.js](https://github.com/SalesforceCloudServices/heroku-usage-plugin/blob/v1.3.0/src/commands/usage/status/dyno.js)_
 <!-- commandsstop -->
---
+++ src/commands/usage/status/attachment.js
@@ -0,0 +1,134 @@
+
+/* eslint-disable no-unused-vars */
+const {Command, flags} = require('@heroku-cli/command');
+const {CustomColors, color} = require('@heroku-cli/color');
+const {ux} = require('cli-ux');
+const cli = require('heroku-cli-util');
+
+const path = require('path');
+const fs = require('fs-extra');
+/* eslint-enable no-unused-vars */
+
+const StatusCommand = require('./'); // eslint-disable-line unicorn/import-index
+const AppsCommand = require('../list/apps');
+// const ObjectUtil = require('../../../modules/ObjectUtil');
+const PrintUtil = require('../../../modules/PrintUtil');
+
+class AddonStatusCommand extends StatusCommand {
+  async run() {
+    let results;
+    let jsonResults = {};
+    const {flags: commandFlags} = this.parse(AddonStatusCommand);
+    const appFlag = commandFlags.app || null;
+    const user = commandFlags.user || null;
+    const team = commandFlags.team || null;
+    const format = commandFlags.format || 'human';
+    const cache = commandFlags.cache || false;
+
+    const tableFormat = {
+      csv: format === 'csv',
+      'no-truncate': format === 'csv'
+    };
+    const useIso = format !== 'human';
+    let appList;
+
+    const localizeDate = PrintUtil.printDate.bind(null, useIso);
+    const localizeNumber = PrintUtil.printNumber.bind(null, useIso);
+    const printPlan = PrintUtil.printHerokuCostPlan;
+    const printUnit = PrintUtil.printHerokuCostUnit;
+    const printCost = PrintUtil.printHerokuCostPrice.bind(null, useIso);
+
+    if (appFlag) {
+      appList = appFlag.split(/\s*,\s*/);
+    } else {
+      let args = ['-s'];
+      if (team) {
+        args = [...args, '-t', team];
+      } else if (user) {
+        args = [...args, '-u', user];
+      }
+      appList = await AppsCommand.run(args);
+    }
+
+    results = {};
+    jsonResults = {};
+
+    results = await this.getStatus(appList, true, false, cache);
+
+    let timestamp = new Date().toISOString();
+    const tableTimestamp = {};
+    if (format !== 'human') tableTimestamp.timestamp = {header: 'Timestamp'};
+
+    jsonResults.attachments = Object.keys(results)
+      .map((key) => results[key])
+      .reduce((attachmentsResults, appInfo) => [
+        ...attachmentsResults,
+        ...appInfo.attachmentsBody.map((attachment) => ({
+          timestamp,
+          attachmentId: attachment.id,
+          attachmentName: attachment.name,
+          addonId: attachment.addon.id,
+          addonName: attachment.addon.name,
+          webURL: attachment.web_url,
+          logInputURL: attachment.log_input_url,
+          namespace: attachment.namespace,
+          owningAppId: attachment.app.id,
+          owningAppName: attachment.app.name,
+          attachedAppId: attachment.addon.app.id,
+          attachedAppName: attachment.addon.app.name,
+          createdAt: localizeDate(attachment.created_at),
+          updatedAt: localizeDate(attachment.updated_at),
+          relationship: attachment.addon.app.id === attachment.app.id ? 'direct' : 'attachment'
+        }))
+      ], []);
+
+    if (format === 'json') {
+      cli.log(JSON.stringify(jsonResults, null, 2));
+    } else {
+      ux.table(jsonResults.attachments, {
+        attachmentName: {header: 'Attachment Name'},
+        addonName: {header: 'Add-On Name'},
+        // attachmentId: {header: ''},
+        relationship: {header: 'Relationship'},
+        // addonId: {header: ''},
+        // webURL: {header: ''},
+        // logInputURL: {header: ''},
+        // owningAppId: {header: ''},
+        owningAppName: {header: 'Owning App'},
+        // attachedAppId: {header: ''},
+        attachedAppName: {header: 'Attached App'},
+        updatedAt: {header: 'Updated At'},
+        createdAt: {header: 'Created At'},
+        ...tableTimestamp
+      }, tableFormat);
+    }
+
+    return Promise.resolve(results);
+  }
+}
+
+AddonStatusCommand.description = `List of Add-Ons at the attachment level.
+
+(Please note that if neither a team or user is specified, all apps for the current user are considered)
+
+Attachment Name: Unique name for this add-on attachment to this app
+Add-On Name: Globally unique name of the Add-On
+Relationship: whether the attachment is owned by a different app (attached) or directly by the app (direct)
+Owning App: Name of the singular app that owns the Add-On
+Attached App: Name of the app that this add-on is attached to
+Created At: the date the attachment was created
+Updated At: the date the attachment was updated
+
+For more information, please see:
+https://devcenter.heroku.com/articles/platform-api-reference#add-on-attachment
+`;
+
+AddonStatusCommand.flags = {
+  app: flags.string({char: 'a', description: 'comma separated list of app names or ids'}),
+  user: flags.string({char: 'u', description: 'account email or user id'}),
+  team: flags.string({char: 't', description: 'team name or id'}),
+  format: flags.string({char: 'f', description: 'format of output', default: 'human', options: ['human', 'json', 'csv']}),
+  cache: flags.boolean({hidden: true, description: 'store the app results into a cache file ./data.json'})
+};
+
+module.exports = AddonStatusCommand;
